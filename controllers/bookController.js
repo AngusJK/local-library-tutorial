9b16ae9a4e451b57df6127e6053ce07c301f572f
Angus Kellett <anguskellett@hotmail.com>
2021-05-03 21:58:24 +0000

    implements delete_book_get

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -161,7 +161,22 @@ exports.book_create_post = [
 
 // Display book delete form on GET.
 exports.book_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book delete GET');
+
+    async.parallel({
+        book: function(callback) {
+            Book.findById(req.params.id).exec(callback)
+        },
+        book_instance: function(callback) {
+            BookInstance.findById(req.params.id).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) return { next(err); }
+        if (results.book==null) {
+            res.redirect('/catalog/books');
+        }
+        res.render('book_delete', { title: 'Book Delete', book: results.book, book_instance: results.book_instance }); 
+    }
+    )
 };
 
 // Handle book delete on POST.
